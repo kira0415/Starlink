4806aa502f013a7e91e62a45614498ab190a204f
kira0415 <kirahuang0415@gmail.com>
2020-07-27 15:56:47 -0400

    First commit

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+class Footer extends Component {
+    render() {
+        return (
+            <footer className="footer">
+                ©2020 StarLink Tracker. All Rights Reserved. Website Made by Huiquan Huang
+            </footer>
+        )
+    }
+}
+export default Footer;
